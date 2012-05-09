425067d5d5cb2cba25dc8f41f27fc3d55a8edeec
dbridle <darren.bridle@gmail.com>
2012-05-09 19:06:28 +0100

    Commented out race stats

--- PoMLibrary/Race.cs
+++ PoMLibrary/Race.cs
@@ -10,10 +10,10 @@ namespace PoMLibrary
         private string _name = "";
         private string _description = "";
 
-        private Dictionary<string, int> _statModifiers;
+        //private Dictionary<string, int> _statModifiers;
 
-        private List<Modifier> _weaknesses;
-        private List<Modifier> _resistances;
+        //private List<Modifier> _weaknesses;
+        //private List<Modifier> _resistances;
 
         public string Name
         {
@@ -27,33 +27,33 @@ namespace PoMLibrary
             set {if (!string.IsNullOrEmpty(value)) _description = value;}
         }
 
-        public Dictionary<string, int> StatModifiers
-        {
-            get {return _statModifiers; }
-            set {_statModifiers = value; }
-        }
+        //public Dictionary<string, int> StatModifiers
+        //{
+        //    get {return _statModifiers; }
+        //    set {_statModifiers = value; }
+        //}
 
-        public List<Modifier> Weaknesses
-        {
-            get {return _weaknesses; }
-            set {_weaknesses = value; }
-        }
+        //public List<Modifier> Weaknesses
+        //{
+        //    get {return _weaknesses; }
+        //    set {_weaknesses = value; }
+        //}
 
-        public List<Modifier> Resistances
-        {
-            get {return _resistances; }
-            set {_resistances = value; }
-        }
+        //public List<Modifier> Resistances
+        //{
+        //    get {return _resistances; }
+        //    set {_resistances = value; }
+        //}
 
-        public int StatModifiersCount
-        {
-            get {return (_statModifiers != null) ? _statModifiers.Count : 0; }
-        }
+        //public int StatModifiersCount
+        //{
+        //    get {return (_statModifiers != null) ? _statModifiers.Count : 0; }
+        //}
 
-        public int WRsCount
-        {
-            get { return ((_resistances != null) ? _resistances.Count : 0) + ((_weaknesses != null) ? _weaknesses.Count : 0); }
-        }
+        //public int WRsCount
+        //{
+        //    get { return ((_resistances != null) ? _resistances.Count : 0) + ((_weaknesses != null) ? _weaknesses.Count : 0); }
+        //}
 
         public Race()
         {
@@ -64,113 +64,113 @@ namespace PoMLibrary
             _name = name;
         }
 
-        #region Stat Modifier Functions
+        //#region Stat Modifier Functions
 
-        public int GetStatModifier(string abbr)
-        {
-            int value = 0;
+        //public int GetStatModifier(string abbr)
+        //{
+        //    int value = 0;
 
-            if (_statModifiers != null)
-                _statModifiers.TryGetValue(abbr, out value);
+        //    if (_statModifiers != null)
+        //        _statModifiers.TryGetValue(abbr, out value);
 
-            return value;
-        }
+        //    return value;
+        //}
 
-        public void AddStatModifier(string abbr, int value)
-        {
-            if (_statModifiers == null)
-                _statModifiers = new Dictionary<string, int>();
+        //public void AddStatModifier(string abbr, int value)
+        //{
+        //    if (_statModifiers == null)
+        //        _statModifiers = new Dictionary<string, int>();
 
-            _statModifiers.Add(abbr, value);
-        }
+        //    _statModifiers.Add(abbr, value);
+        //}
 
-        public void RemoveStatModifier(string abbr)
-        {
-            if (_statModifiers == null)
-                return;
+        //public void RemoveStatModifier(string abbr)
+        //{
+        //    if (_statModifiers == null)
+        //        return;
 
-            _statModifiers.Remove(abbr);
-        }
+        //    _statModifiers.Remove(abbr);
+        //}
 
-        public Dictionary<string, int> GetStatMods()
-        {
-            return _statModifiers;
-        }
+        //public Dictionary<string, int> GetStatMods()
+        //{
+        //    return _statModifiers;
+        //}
 
-        #endregion
+        //#endregion
 
-        #region Weakness/Resistances functions
+        //#region Weakness/Resistances functions
 
-        public void AddWeakness(Modifier weakness)
-        {
-            if (_weaknesses == null)
-                _weaknesses = new List<Modifier>();
+        //public void AddWeakness(Modifier weakness)
+        //{
+        //    if (_weaknesses == null)
+        //        _weaknesses = new List<Modifier>();
 
-            _weaknesses.Add(weakness);
-        }
+        //    _weaknesses.Add(weakness);
+        //}
 
-        public void AddResistance(Modifier resistance)
-        {
-            if (_resistances == null)
-                _resistances = new List<Modifier>();
+        //public void AddResistance(Modifier resistance)
+        //{
+        //    if (_resistances == null)
+        //        _resistances = new List<Modifier>();
 
-            _resistances.Add(resistance);
-        }
+        //    _resistances.Add(resistance);
+        //}
 
-        public void RemoveWeakness(int index)
-        {
-            if (_weaknesses == null)
-                return;
+        //public void RemoveWeakness(int index)
+        //{
+        //    if (_weaknesses == null)
+        //        return;
 
-            _weaknesses.RemoveAt(index);
-        }
+        //    _weaknesses.RemoveAt(index);
+        //}
 
-        public void RemoveResistance(int index)
-        {
-            if (_resistances == null)
-                return;
+        //public void RemoveResistance(int index)
+        //{
+        //    if (_resistances == null)
+        //        return;
 
-            _resistances.RemoveAt(index);
-        }
+        //    _resistances.RemoveAt(index);
+        //}
 
-        public int CheckResistnance(ModifierType type)
-        {
-            int amount = 0;
+        //public int CheckResistnance(ModifierType type)
+        //{
+        //    int amount = 0;
 
-            if (_resistances != null)
-                foreach (Modifier resistance in _resistances)
-                    if (resistance.Type == type)
-                        amount = resistance.Amount;
+        //    if (_resistances != null)
+        //        foreach (Modifier resistance in _resistances)
+        //            if (resistance.Type == type)
+        //                amount = resistance.Amount;
 
-            return amount;
-        }
+        //    return amount;
+        //}
 
-        public int CheckWeakness(ModifierType type)
-        {
-            int amount = 0;
+        //public int CheckWeakness(ModifierType type)
+        //{
+        //    int amount = 0;
 
-            if (_weaknesses != null)
-                foreach (Modifier weakness in _weaknesses)
-                    if (weakness.Type == type)
-                        amount = weakness.Amount;
+        //    if (_weaknesses != null)
+        //        foreach (Modifier weakness in _weaknesses)
+        //            if (weakness.Type == type)
+        //                amount = weakness.Amount;
 
-            return amount;
-        }
+        //    return amount;
+        //}
 
-        public void ClearStatModifiers()
-        {
-            _statModifiers.Clear();
-        }
+        //public void ClearStatModifiers()
+        //{
+        //    _statModifiers.Clear();
+        //}
 
-        public void ClearWRs()
-        {
-            if (_resistances != null)
-                _resistances.Clear();
-            if (_weaknesses != null)
-                _weaknesses.Clear();
-        }
+        //public void ClearWRs()
+        //{
+        //    if (_resistances != null)
+        //        _resistances.Clear();
+        //    if (_weaknesses != null)
+        //        _weaknesses.Clear();
+        //}
 
-        #endregion
+        //#endregion
 
     }
 }
